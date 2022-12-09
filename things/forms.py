fd03b7c3f42a61826131ce1933b18f4887e3145b
Echlas Malik <99184338+EchlasMalik@users.noreply.github.com>
2022-12-09 15:59:52 +0000

    form made

--- things/forms.py
+++ things/forms.py
@@ -1,3 +1,9 @@
 """Forms of the project."""
 
 # Create your forms here.
+
+class ThingForm(forms.form):
+    name = forms.charField(label="Fullname")
+    description = forms.charField(label="Description")
+    Quantity = forms.charField(label="quantity")
+    
